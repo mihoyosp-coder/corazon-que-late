87b7e6e8819780c040c36193ef66106e1cc6439c
mihoyosp-coder <mihoyosp@ut.edu.co>
2026-03-09 15:40:33 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.py
@@ -0,0 +1,8 @@
+basic.show_leds("""
+    . # . # .
+    # # # # #
+    # # # # #
+    . # # # .
+    . . # . .
+    """)
+basic.show_string("DANNA😁")
---
+++ main.ts
@@ -0,0 +1,13 @@
+music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
+basic.showLeds(`
+    . # # # .
+    # # # # #
+    # # # # #
+    . # # # .
+    . . # . .
+    `)
+music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
+music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
+basic.showString("DANNA😁")
+music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
+basic.showIcon(IconNames.Happy)
